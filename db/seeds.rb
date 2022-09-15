0f9389def26e5a1c6b6c534d476623d97909f724
Kasper Høglund <kasper@rubynor.no>
2022-09-15 13:45:15 +0200

    event model added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+5.times do |i|
+  Event.create(title: "Huge event!!", description: "I cannot stress dis enougth!!!", datetime: "2001-02-03T04:05:06", location: "Baker's street")
+end
