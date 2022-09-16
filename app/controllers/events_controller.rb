894b21654fe7266f5240f6cfbb5fcdefa40b6898
Kasper Høglund <kasper@rubynor.no>
2022-09-16 14:41:41 +0200

    events#show added

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,6 +3,10 @@ class EventsController < ApplicationController
     @events = Event.all
   end
 
+  def show
+    @event = Event.find(params[:id])
+  end
+
   before_action :authenticate_user!
 
   def new
