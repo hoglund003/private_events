367b57cd95de40dc06798c1e8bbcefcaa4e78de9
Kasper Høglund <kasper@rubynor.no>
2022-09-16 14:28:07 +0200

    events#new + events#create added

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,27 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  before_action :authenticate_user!
+
+  def new
+    @event = Event.new
+  end
+  
+  def create
+    @event = current_user.events.build(event_params)
+    if @event.save
+      flash[:notice] = "You created an event!"
+      redirect_to events_path
+    else
+      flash.now[:alert] = "Event not created!"
+      render :new
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :description, :datetime, :location)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'users/show'
   devise_for :users, :path => 'u'
   resources :users
-  resources :events, only: [:index, :show]
+  resources :events, only: [:index, :show, :new, :create]
   root "events#index"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
