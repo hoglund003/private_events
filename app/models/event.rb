c67457d65dc398b474f732ff33217a6315c83fd9
Kasper Høglund <kasper@rubynor.no>
2022-09-19 14:15:45 +0200

    indecate past or upcoming event on user page

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,14 @@ class Event < ApplicationRecord
 
   has_many :event_attendees
   has_many :attendees, through: :event_attendees, source: :user
+
+  scope :past, -> { where("datetime < ?", Time.now) }
+  scope :upcoming, -> { where("datetime > ?", Time.now) }
+  # def self.past
+  #   Event.all.select {|e| e.datetime < Time.now}
+  # end
+  
+  # def self.upcoming
+  #   Event.all.select {|e| e.datetime > Time.now}
+  # end
 end
